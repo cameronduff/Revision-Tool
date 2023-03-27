debcbf90f16987bc7173aa1c0a06572d7cb621fb
cameronduff <cameronsduff@gmail.com>
2023-03-27 19:52:18 +0100

    Base Commit

---
+++ celestial/src/test/java/com/celestial/celestial/CelestialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celestial.celestial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
